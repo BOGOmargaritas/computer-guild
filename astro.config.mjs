f4e65185834c3181d7181b4e43321c90a53f1d28
Cullen Abbs <cullen0880@gmail.com>
2024-11-13 22:06:14 -0500

    Add audio player component and integrate with game details; update TypeScript config and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import vercel from "@astrojs/vercel/serverless";
 
 import svelte from "@astrojs/svelte";
 
+import mdx from "@astrojs/mdx";
+
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
+  integrations: [svelte(), mdx(), react()],
   output: "hybrid",
   adapter: vercel()
 });
